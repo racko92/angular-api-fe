be2a3297f841b6ecc78bb2e89a0c041799f9e025
racko92 <vladimir.rackovic.92@gmail.com>
2017-09-06 20:01:14 +0200

    deleteContact works

--- src/app/shared/services/contacts.service.ts
+++ src/app/shared/services/contacts.service.ts
@@ -66,11 +66,17 @@ export class ContactsService {
   public removeContact(contact: Contact)
   {
     return new Observable((o: Observer<any>) => {
-      const index = this.contacts.indexOf(contact);
-      this.contacts.splice(index, 1);
+      
 
-      o.next(index);
-      return o.complete();
+      this.http.delete('http://localhost:8000/api/contacts/' + contact.id)
+      .subscribe(() => {
+
+          const index = this.contacts.indexOf(contact);
+          this.contacts.splice(index, 1);
+
+          o.next(index);
+          return o.complete();
+      })
     });
   }
 
