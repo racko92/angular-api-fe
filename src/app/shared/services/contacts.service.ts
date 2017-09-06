cabc148bbb994d7542a9612d6cf4f42edb619b97
racko92 <vladimir.rackovic.92@gmail.com>
2017-09-06 20:11:14 +0200

    getContactById povezan bez error-a

--- src/app/shared/services/contacts.service.ts
+++ src/app/shared/services/contacts.service.ts
@@ -82,14 +82,20 @@ export class ContactsService {
 
   public getContactById(id: number)
   {
-    return new Observable((o: Observer<any>) => {
-      let existing = this.contacts.filter(c => c.id == id);
-      if (existing.length) {
-        o.next(existing);
-        return o.complete();
-      } else {
-        return o.error('Not found');
-      }
+    return new Observable((o: Observer<any>) => { 
+      // else {
+      //   return o.error('Not found');
+      // }
+
+      this.http.get('http://localhost:8000/api/contacts/' + id).subscribe((contact: any) => {
+
+          let existing = this.contacts.filter(c => c.id == id);
+          if (existing.length) {
+            o.next(existing[0]);
+            return o.complete();
+          }
+
+      })
     });
   }
 
